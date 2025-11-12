61a026c2f7b521453f1d5ea657f8e37ed3aca7e1
rafia_haque_arpita <rafia.haque.cse@ulab.edu.bd>
2025-11-13 02:39:47 +0600

    Login screen UI added

---
+++ lib/login_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LoginnScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(height: 100),
+                Text(
+                  "Ulab Smart Lost and Found System \n Management System",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 100),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "ULAB ID",
+                    filled: true,
+                    prefixIcon: Icon(Icons.person),
+                    fillColor: Colors.white.withOpacity(0.5),
+                    hintStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
