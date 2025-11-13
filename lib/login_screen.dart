6987f45fb9592438fe7b5693a58daf357d9bb2a9
rafia_haque_arpita <rafia.haque.cse@ulab.edu.bd>
2025-11-13 23:48:54 +0600

    Login screen UI added

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -50,3 +50,24 @@ class LoginnScreen extends StatelessWidget {
                     ),
                   ),
                 ),
+                  ),
+                TextField(
+                  decoration: InputDecoration(
+                    hintText: "ULAB ID",
+                    filled: true,
+                    prefixIcon: Icon(Icons.perm_identity),
+                    fillColor: Colors.white.withOpacity(0.5),
+                    hintStyle: TextStyle(color: Colors.black),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
